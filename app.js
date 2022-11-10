0db1346d96798914811e82ba1676263f005ccc03
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2022-11-10 08:01:37 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+// var arr = [
+//     arr1= "Abdul",
+//     arr2="Rehman",
+//     arr3= "A.Rehman"
+
+// ]
+// console.log(arr)
+
+// var obj ={
+//     arr1: ["Abdul"],
+//     arr2: ["Rehman"],
+//     arr3: ["A.Rehman"],
+
+// }
+// console.log(obj)
+
+
+// var arr =["Abdul","Rehman","A.Rehman"];
+// console.log(arr)
+
+// var arr =[1,2 ,"Abdul","Rehman","A.Rehman",3,4,5,];
+// console.log(arr)
+
+// var arr =[1,2,3,4,5,];
+// console.log(arr)
+
+// var arr = ["Zong","Moblink","Warid","Telenor"]
+// console.log(arr)
+
+var arr = ["SSC", "HSC","BCS","BS","BCOM","MS","M.Phil","Phd"]
+for( var i=0 ;i<arr.length;i++){
+    console.log(i + arr )
+}
